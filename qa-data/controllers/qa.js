a135b93d17e39e1f80d175be7250c5bbecd2ed85
irenaSh <irena@cd.com.au>
2015-01-02 23:20:46 -0800

    Add support for Firebase, implement adding ticket.

--- qa-data/controllers/qa.js
+++ qa-data/controllers/qa.js
@@ -1,13 +1,27 @@
 angular.module("qa")
-    .controller("qaCtrl", function ($scope, $http) {
+    .controller("qaCtrl", function ($scope, $http, $firebase) {
         $scope.data = {};
 
-        $http.get("qa-data.json")
-            .success(function(data) {
-                $scope.data.qa = data;
-            })
-            .error(function(error) {
-                $scope.data.error = error;
-                console.log("Error retrieving data: " + error);
-            });
+        var ref = new Firebase("https://amber-heat-6599.firebaseio.com/");
+        $scope.data.qa = $firebase(ref).$asArray();
+
+        $scope.addTicket = function () {
+            var newTicket = $scope.data.newTicket;
+
+            if (!newTicket.question) {
+                return;
+            }
+
+            newTicket.id = $scope.data.qa.length + 1;
+
+            $scope.data.qa.$add(newTicket)
+                .then(function(ref) {
+                    console.log("Added new question with id " + ref.key());
+                }, function(error) {
+                    console.log("Error:", error);
+                });
+
+            $scope.data.newTicket = '';
+        };
+
     });
